8cbbece332cc0d2e981167aca39f6f6e159f09e9
Chrisowsky <krzstr98@gmail.com>
2023-05-11 18:59:19 +0200

    Added Fifth C++ Program

---
+++ Odliczanie-Piaty-Program/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int wiek;
+
+int main()
+{
+      for(int i=15; i>=0; i--)
+      {
+            cout<<i;
+            Sleep(1000);
+            system("cls");
+      }
+      cout<<"START!";
+
+      return 0;
+}
